9eed11ba3dcca00b3ebcfcb18ad14453458e0e9b
Catalinapalma <cata.palmam@duocuc.cl>
2026-06-04 20:04:19 -0400

    feat: integración inicial JavaCV y Haar Cascade

--- backend/src/main/java/com/servigo/servigo/service/JavaCvService.java
+++ backend/src/main/java/com/servigo/servigo/service/JavaCvService.java
@@ -1,15 +1,25 @@
 package com.servigo.servigo.service;
 
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class JavaCvService {
 
-public Double compararRostros(
+    public Double compararRostros(
             String urlFotoReferencia,
             MultipartFile fotoCapturada
     ) {
         return 85.0;
+    }
+
+    public boolean detectarRostro(MultipartFile foto) {
+
+        CascadeClassifier detector = new CascadeClassifier();
+
+        System.out.println(detector);
+
+        return true;
     }
 }
