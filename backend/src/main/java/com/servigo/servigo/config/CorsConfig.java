7226ea0be21872251f7f23a62289cee764b6e803
catalinaPalma <cata.palmam@duocuc.cl>
2026-06-26 20:00:23 -0400

    modificacion a corsconfig y agrega vercel.json

--- backend/src/main/java/com/servigo/servigo/config/CorsConfig.java
+++ backend/src/main/java/com/servigo/servigo/config/CorsConfig.java
@@ -21,7 +21,8 @@ public class CorsConfig {
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://localhost:5174",
-                "http://127.0.0.1:5174"
+                "http://127.0.0.1:5174",
+                "https://frontend-servigo.vercel.app"
         ));
 
         config.setAllowedMethods(List.of(
